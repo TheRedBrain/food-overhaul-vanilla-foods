532d7e4b7266219aab78225996a6f581e1114b7b
TheRedBrain <thetrueredbrain@gmail.com>
2025-10-26 02:01:47 +0100

    fixed block registry

--- src/main/java/com/github/theredbrain/foodoverhaulvanillafoods/registry/BlockRegistry.java
+++ src/main/java/com/github/theredbrain/foodoverhaulvanillafoods/registry/BlockRegistry.java
@@ -1,14 +1,17 @@
 package com.github.theredbrain.foodoverhaulvanillafoods.registry;
 
 import com.github.theredbrain.foodoverhaul.FoodOverhaul;
+import com.github.theredbrain.foodoverhaul.block.entity.FoodBlockEntity;
 import com.github.theredbrain.foodoverhaul.registry.EntityRegistry;
 import com.github.theredbrain.foodoverhaulvanillafoods.FoodOverhaulVanillaFoods;
 import com.github.theredbrain.foodoverhaulvanillafoods.block.OverhauledCakeBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.piston.PistonBehavior;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.NbtComponent;
+import net.minecraft.entity.TypedEntityData;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.nbt.NbtCompound;
@@ -29,11 +32,11 @@ public class BlockRegistry {
 		default_cake_nbt.putBoolean("ambient", false);
 		default_cake_nbt.putBoolean("showParticles", false);
 		default_cake_nbt.putBoolean("infiniteUses", false);
-
+		TypedEntityData<BlockEntityType<?>> default_cake_data = TypedEntityData.create(EntityRegistry.GENERIC_FOOD_BLOCK_ENTITY, default_cake_nbt);
 		OVERHAULED_CAKE = FoodOverhaul.registerFoodBlock(
 				FoodOverhaulVanillaFoods.identifier("overhauled_cake"),
 				new OverhauledCakeBlock(AbstractBlock.Settings.create().solid().strength(0.5F).sounds(BlockSoundGroup.WOOL).pistonBehavior(PistonBehavior.DESTROY)),
-				new Item.Settings().component(DataComponentTypes.BLOCK_ENTITY_DATA, NbtComponent.of(default_cake_nbt)).maxCount(1),
+				new Item.Settings().component(DataComponentTypes.BLOCK_ENTITY_DATA, default_cake_data).maxCount(1),
 				ItemGroups.FOOD_AND_DRINK
 		);
 	}
